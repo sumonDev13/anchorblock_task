5f0c5981d86f64621afb1d1b0edc463f7bd269c4
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-16 01:16:05 +0600

    font added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Users from './pages/Users';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Users from "./pages/Users";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  
-
   return (
     <BrowserRouter>
-  
-    <Routes>
-      <Route path='/' element={<Login name={''} />} />
-      <Route path='/signup' element={<Signup name={''} />} />
-      <Route path='/users' element={<Users name={''} />} />
-      <Route path='/dashboard' element={<Dashboard name={''} />} />
-     
-      
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<Login name={""} />} />
+        <Route path="/signup" element={<Signup name={""} />} />
+        <Route path="/users" element={<Users name={""} />} />
+        <Route path="/dashboard" element={<Dashboard name={""} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
