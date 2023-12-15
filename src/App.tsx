91a79b80f539d711c0bbf3d197ed3db8376c8215
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-16 00:58:52 +0600

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Users from './pages/Users'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Users from './pages/Users';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   
 
   return (
-   <div>
-    <Login name={''}/>
-    <Signup name={''}/>
-    <Dashboard name={''}/>
-    <Users name={''}/>
-   </div>
+    <BrowserRouter>
+  
+    <Routes>
+      <Route path='/' element={<Login name={''} />} />
+      <Route path='/signup' element={<Signup name={''} />} />
+      <Route path='/users' element={<Users name={''} />} />
+      <Route path='/dashboard' element={<Dashboard name={''} />} />
+     
+      
+    </Routes>
+  </BrowserRouter>
   )
 }
 
