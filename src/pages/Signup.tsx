d993881ede976f667f35b105de2e8b0233b572de
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-16 01:33:12 +0600

    logo updated

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -9,11 +9,11 @@ const Signup: React.FC<signup> = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src="/src/assets/icons/signup_logo.svg"
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Sign up to join with Stack
           </h2>
         </div>
 
