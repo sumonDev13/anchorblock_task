245ed3d11d7902e8ec8d7b603033a83286f6896f
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-16 01:00:45 +0600

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Users from './pages/Users';
