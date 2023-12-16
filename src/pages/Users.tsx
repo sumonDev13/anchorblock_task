350b6192365e7b0e911a3a67c2ffa27f29357bec
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-17 01:07:02 +0600

    table

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -9,32 +9,32 @@ const Users: React.FC<IUsers> = () => {
     <>
       <Navbar name={""} />
       <div className="max-w-[1240px] mx-auto p-4">
-      <div className="max-h-[500px] relative ">
-        <div className="flex justify-between p-4">
-          <div className="flex items-center">
-            <h2 className="text-2xl text-levelBlack font-bold">Users</h2>
-          </div>
-          <div className="flex items-center space-x-4">
-            <button className="flex items-center px-4 py-2 bg-white text-customBlack border border-gray-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-            <img
-                    className="mr-2"
-                    src="/src/assets/icons/upload-cloud.svg"
-                    alt="Your Company"
-                  />
-              Import
-            </button>
-            <button className="flex items-center px-4 py-2 bg-customPurple text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
-            <img
-                    className="mr-2"
-                    src="/src/assets/icons/plus.svg"
-                    alt="Your Company"
-                  />
-              Add User
-            </button>
+        <div className="max-h-[500px] relative ">
+          <div className="flex justify-between p-4">
+            <div className="flex items-center">
+              <h2 className="text-2xl text-levelBlack font-bold">Users</h2>
+            </div>
+            <div className="flex items-center space-x-4">
+              <button className="flex items-center px-4 py-2 bg-white text-customBlack border border-gray-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                <img
+                  className="mr-2"
+                  src="/src/assets/icons/upload-cloud.svg"
+                  alt="Your Company"
+                />
+                Import
+              </button>
+              <button className="flex items-center px-4 py-2 bg-customPurple text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
+                <img
+                  className="mr-2"
+                  src="/src/assets/icons/plus.svg"
+                  alt="Your Company"
+                />
+                Add User
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
