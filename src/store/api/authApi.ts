de6a8159d0b6500a41ac70ad1e4073eca6686988
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-17 20:08:21 +0600

    RTK query setup

---
+++ src/store/api/authApi.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import type { Pokemon } from './types'
+
+// Define a service using a base URL and expected endpoints
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://reqres.in/" }),
+  endpoints: (builder) => ({
+    signinUser: builder.mutation({
+      query: (body: { email: string; password: string }) => {
+        return {
+          url: "/user/signin",
+          method: "post",
+          body,
+        };
+      },
+    }),
+    signupUser: builder.mutation({
+      query: (body: { name: string; email: string; password: string }) => {
+        return {
+          url: "/user/signup",
+          method: "post",
+          body,
+        };
+      },
+    }),
+
+    verifyUser: builder.mutation({
+      query: (body: { token: string }) => {
+        console.log(body.token);
+
+        return {
+          url: "/user/verfiy-user-mail",
+          method: "post",
+          body,
+        };
+      },
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useSigninUserMutation,useSignupUserMutation,useVerifyUserMutation} = authApi;
